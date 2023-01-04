d67641bdeb1c882f0c18b951ad7066c58ce791e2
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2023-01-04 23:53:11 +0100

    add function initActions

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,50 @@
-'use strict';
+{
+  'use strict';
 
-const select = {
-  templateOf: {
-    book: '#template-book',
-  },
-  containerOf: {
-    booksList: '.books-list',
-  },
-};
+  const select = {
+    bookSzablon: {
+      template: '#template-book',
+    },
+    bookList: {
+      lista: '.books-list',
+    },
+    book: {
+      image: '.books-list .book__image'
+    }
+  };
 
-// eslint-disable-next-line no-unused-vars
-const templates = {
-  books: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
-};
+  const className = {
+    favorite: 'favorite',
+  };
 
-function renderBooks(){
-  for(let book of dataSource.books){
-    const generatedHTML = templates.books(book);
-    const generatedDOM = utils.createDOMFromHTML(generatedHTML);
-    const menuContainer = document.querySelector(select.containerOf.booksList);
-    menuContainer.appendChild(generatedDOM);
+  // eslint-disable-next-line no-unused-vars
+  const templates = {
+    SzablonBook: Handlebars.compile(document.querySelector(select.bookSzablon.template).innerHTML),
+  };
+
+  function renderBooks(){
+    for(let book of dataSource.books){
+      const generatedHTML = templates.SzablonBook(book);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      const containerMenu = document.querySelector(select.bookList.lista);
+      containerMenu.appendChild(generatedDOM);
+    }
   }
+  const favoriteBooks = [];
+
+  function initActions(){
+    const books = document.querySelectorAll(select.book.image);
+    for(let book of books){
+      book.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        book.classList.add(className.favorite);
+        const clickedBook = book.getAttribute('data-id');
+        favoriteBooks.push(clickedBook);
+      });
+    }
+  }
+
+  renderBooks();
+  initActions();
+
 }
-
-renderBooks();
-
